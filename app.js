e12e189580a76ab63d70d7ae426f61d99c57aa4b
AribSheikh <aribsheikh0@gmail.com>
2023-04-12 20:30:11 +0500

    final

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+var bodyParser = require('body-parser')
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 const port = 3000;
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.use(express.json());
+
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  let content = JSON.parse(fs.readFileSync('notes.json', 'utf8'));
+  // console.log(content)
+  res.render('index', { data: content });
+});
+
+
+app.post('/add', urlencodedParser, (req, res) => {
+  const obj = JSON.parse(JSON.stringify(req.body));
+  let content = JSON.parse(fs.readFileSync('notes.json', 'utf8'));
+  content.unshift(obj.newnote);
+  fs.writeFileSync('notes.json', JSON.stringify(content));
+  res.writeHead(301, { "Location": "/" });
+  return res.end();
+});
+app.post('/delete', (req, res) => {
+  const obj = req.body;
+  var content = JSON.parse(fs.readFileSync('notes.json', 'utf8'));
+  content.splice(obj.id, 1);
+  fs.writeFileSync('notes.json', JSON.stringify(content));
+  res.send(200)
 });
 
 app.listen(port, () => {
